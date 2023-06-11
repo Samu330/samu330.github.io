0fa338eca14ae6099bd916fe431ad3e39e377c60
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2023-06-11 12:58:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-let box = document.querySelector("div"),
-input = document.querySelector("input");
 
-input.addEventListener("input", () => {
-    box.style.borderRadius = input.value;
-    box.style.background = input.value;
-});
