9940ba32d918837c3254737159be2415b145112c
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2023-06-19 19:59:31 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,104 @@
+function getAllRequest()
+{
+  axios.get('http://localhost:8080/item')
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function getFilteredRequest()
+{
+  axios.get('http://localhost:8080/item', {
+    params: {
+      filter : 'myFilter'
+    }
+    })
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function getByIdRequest()
+{
+  id = 10;
+  axios.get('http://localhost:8080/item/' + id)
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function postRequest()
+{
+  axios.post('http://localhost:8080/item', {
+    data: 'NewItem'
+    })
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function putRequest()
+{
+  id = 10;
+  axios.put('http://localhost:8080/item/' + id, {
+    data: 'NewItem'
+    })
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function patchRequest()
+{
+  id = 10;
+  axios.patch('http://localhost:8080/item/' + id, {
+    data: 'NewItem'
+    })
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    })
+    .then(function () {
+    });
+}
+
+function deleteRequest()
+{
+  id = 10;
+  axios.delete('http://localhost:8080/item/' + id)
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    })
+    .then(function () {
+    });
+}
